367bc3253064441be4241941fcdaf676f35a2467
zhangxiaohu <zhangxiaohu@nti56.com>
2022-12-26 17:46:52 +0800

    无，仅测试

--- BitwiseOperation/Test1.java
+++ BitwiseOperation/Test1.java
@@ -5,5 +5,7 @@ public class Test1 {
         int a = 10 ^ 9;
         System.out.println(a);
         System.out.println(2 & 1);
+        int a1 = 1 & 2;
+        System.out.println("a1 " + a1);
     }
 }
---
+++ JavaEE/String/Collectors/Main.java
@@ -0,0 +1,22 @@
+package JavaEE.String.Collectors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> menu = Arrays.asList(
+                new Student("刘一", 721, true, Student.GradeType.THREE),
+                new Student("陈二", 637, true, Student.GradeType.THREE),
+                new Student("张三", 666, true, Student.GradeType.THREE),
+                new Student("李四", 531, true, Student.GradeType.TWO),
+                new Student("王五", 483, false, Student.GradeType.THREE),
+                new Student("赵六", 367, true, Student.GradeType.THREE),
+                new Student("孙七", 499, false, Student.GradeType.ONE));
+        Double collect = menu.stream().collect(Collectors.averagingDouble(Student::getTotalScore));
+        Optional.ofNullable(collect).ifPresent(System.out::println);
+    }
+}
