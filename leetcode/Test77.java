a2799392b016f1944b0372c33122040ab6209774
zhangxiaohu <zhangxiaohu@nti56.com>
2023-02-16 09:48:47 +0800

    Test2341

---
+++ leetcode/Test77.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.List;
+
+public class Test77 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution77{
+    public List<List<Integer>> combine(int n,int k){
+        return null;
+    }
+}
