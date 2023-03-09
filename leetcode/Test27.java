e721d97417a3a736a6c21687171a9661b188e982
张 <55148768+zxhlkw233@users.noreply.github.com>
2023-03-09 19:33:24 +0800

    Test260

---
+++ leetcode/Test27.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Test27 {
+    public static void main(String[] args) {
+        Solution27 solution27 = new Solution27();
+        int i = solution27.removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+        System.out.println("i:" + i);
+    }
+}
+class Solution27 {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        for(int right = 0; right < nums.length;right++){
+            if(nums[right] != val){
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+}
+
--- leetcode/Test66.java
+++ leetcode/Test66.java
@@ -1,24 +1,13 @@
 package leetcode;
-
-import java.math.BigDecimal;
-
+//力扣题66.加一
 public class Test66 {
     public static void main(String[] args) {
-        int a = 4,b = 333;
-        int i = a / b;
-        double c = a / b;
-        System.out.println("i " + i);
-        System.out.println("c " + c);
-        int ii = a % b;
-        double cc = a % b;
-        System.out.println("ii " + ii);
-        System.out.println("cc " + cc);
+        Solution66 solution66 = new Solution66();
+        solution66.plusOne(new int[]{4,3,2,2});
+    }
+}
+class Solution66 {
+    public int[] plusOne(int[] digits) {
 
-        double aa = 4,bb = 333;
-        System.out.println(aa / bb);
-
-        BigDecimal bigA=new BigDecimal(1);
-        BigDecimal bigB=new BigDecimal(3);
-        System.out.println("a/b="+bigA.divide(bigB));
     }
 }
