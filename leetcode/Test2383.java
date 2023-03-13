a4b08cc934487c0e5c07d4bf2bf190d60b726d5d
zhangxiaohu <zhangxiaohu@nti56.com>
2023-03-13 15:27:34 +0800

    Test500

--- leetcode/Test2383.java
+++ leetcode/Test2383.java
@@ -1,5 +1,25 @@
 package leetcode;
 
-public class Test2383 {
-}
+import java.util.Arrays;
 
+public class Test2383 {
+    public static void main(String[] args) {
+        Solution2383 solution2383 = new Solution2383();
+        solution2383.minNumberOfHours(5,3,new int[]{1,4,3,2},new int[]{2,6,3,1});
+    }
+}
+class Solution2383 {
+    public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
+        int a = 0;
+        int b = 0;
+        a += Arrays.stream(energy).sum() - initialEnergy;
+        for (int i = 0; i < experience.length - 1; i++) {
+            b -= experience[i];
+
+            b += experience[i];
+
+        }
+        System.out.println(a);
+        return 0;
+    }
+}
